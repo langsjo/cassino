90330303f63e517a90960f55650ca520ca4e255b
Roy Långsjö <roy.langsjo@aalto.fi>
2024-02-07 10:35:18 +0200

    Added Card and Deck classes, which represent the deck and the cards in it. Deck class has functionality to shuffle and take the top card.

---
+++ src/main/scala/Deck.scala
@@ -0,0 +1,55 @@
+package joo
+
+import scala.util.Random
+import scala.collection.mutable.Buffer
+
+object Deck:
+  //Buffer that stores cards in the deck
+  private var cards = Buffer[Card]()
+  this.shuffle()
+
+  //Used to initialize deck and reshuffle for new rounds
+  def shuffle(): Unit =
+
+    //Clear buffer in case any cards are still in it
+    this.cards.clear()
+
+    //Loop to add cards 1-13 of all 4 suits
+    for
+      suit <- Vector[Suit](Suit.Spade, Suit.Club, Suit.Heart, Suit.Diamond)
+      faceValue <- 1 to 13
+    do
+      (suit, faceValue) match //Special cards get their special values
+        case (Suit.Spade, 2) => this.cards += Card(suit, faceValue, 15) //2 of Spades
+        case (Suit.Diamond, 10) => this.cards += Card(suit, faceValue, 16) //10 of Diamonds
+        case (_, 1) => this.cards += Card(suit, faceValue, 14) //Aces
+        case _ => this.cards += Card(suit, faceValue, faceValue) //Other cards
+
+    this.cards = Random.shuffle(this.cards) //Shuffle the cards into a random order
+
+  //Remove the first card in the buffer and return it
+  def takeCard(): Card =
+    val cardTaken = this.cards.head
+    this.cards = this.cards.tail
+    cardTaken
+
+  //Represents the deck as a string
+  override def toString: String =
+    var repr = ""
+    for card <- this.cards do
+      val name = card.tableValue match
+        case 1 => "Ace"
+        case 11 => "Jack"
+        case 12 => "Queen"
+        case 13 => "King"
+        case _ => s"${card.tableValue}"
+      repr += s"${name} of ${card.suit}s, "
+
+    repr.dropRight(2)
+
+  //Returns the next card but does not remove it from the deck
+  def next = this.cards.head
+
+
+@main def test() =
+  println(Deck)
