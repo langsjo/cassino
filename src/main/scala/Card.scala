c493ca5110aa884491399409e60e42bb3ca152c4
langsjo <104687438+langsjo@users.noreply.github.com>
2024-02-07 12:12:32 +0200

    Moved part of the toString command that named cards from the Deck object to the Card class.

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -3,7 +3,16 @@ package joo
 //Simple card class to represent cards of the deck
 //tableValue represents the value of the card once placed on the table
 //handValue represents the value of the card when in a player's hand
-case class Card(val suit: Suit, val tableValue: Int, val handValue: Int)
+case class Card(val suit: Suit, val tableValue: Int, val handValue: Int):
+  override def toString: String = 
+     val name = this.tableValue match
+        case 1 => "Ace"
+        case 11 => "Jack"
+        case 12 => "Queen"
+        case 13 => "King"
+        case _ => s"${this.tableValue}"
+        
+     s"${name} of ${this.suit}s"
 
 
 //Suit objects for checking suit of cards
