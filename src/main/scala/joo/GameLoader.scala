5a8f64d4afd9d98e8ad180bf5bf7d0f21c5d2d96
Roy Långsjö <roy.langsjo@aalto.fi>
2024-04-08 11:22:24 +0300

    Added more error handling to loading save files.

--- src/main/scala/joo/GameLoader.scala
+++ src/main/scala/joo/GameLoader.scala
@@ -15,74 +15,78 @@ object GameLoader:
           saveTo += cardReprToCard(cardRepr)
       saveTo.toVector
 
+  def returnAsIntOrThrowError(input: String): Int =
+    if input.forall( x => x.isDigit ) then
+      input.toInt
+    else
+      throw CorruptedSaveException("Save had non-number where a number should be.")
+
   def load(file: File): Option[Game] =
-    try
-      val source = scala.io.Source.fromFile(file)
-      val lines = source.getLines().toVector
 
-      val gameData = lines.head.split('|').map( x => x.split(';') )
-      val generalData = gameData(0)
-      val tableData = gameData(1)
-      val deckData = gameData(2)
+    val source = scala.io.Source.fromFile(file)
+    val lines = source.getLines().toVector
 
-      val turnCount = generalData(0).toInt
-      val dealerCount = generalData(1).toInt
-      val roundNumber = generalData(2).toInt
-      val deckCount = generalData(3).toInt
-      val lastCardTaker = generalData(4)
+    val gameData = lines.head.split('|').map( x => x.split(';') )
+    val generalData = gameData(0)
+    val tableData = gameData(1)
+    val deckData = gameData(2)
 
-      val tableCards = getCardData(tableData)
-      val deckCards = getCardData(deckData)
+    val turnCount = returnAsIntOrThrowError(generalData(0))
+    val dealerCount = returnAsIntOrThrowError(generalData(1))
+    val roundNumber = returnAsIntOrThrowError(generalData(2))
+    val deckCount = returnAsIntOrThrowError(generalData(3))
+    val lastCardTaker = generalData(4)
 
-      val game = Game(deckCount)
+    val tableCards = getCardData(tableData)
+    val deckCards = getCardData(deckData)
 
-      val players = Buffer[Player]()
-      for line <- lines.tail do //all but first (game data) line
-        val data = line.split('|').map( x => x.split(';') )
-        val playerData = data(0)
-        val handData = data(1)
-        val pileData = data(2)
+    val game = Game(deckCount)
 
-        val name = playerData(0)
-        val difficulty = playerData(1)
-        val points = playerData(2).toInt
-        val sweeps = playerData(3).toInt
+    val players = Buffer[Player]()
+    for line <- lines.tail do //all but first (game data) line
+      val data = line.split('|').map( x => x.split(';') )
+      val playerData = data(0)
+      val handData = data(1)
+      val pileData = data(2)
 
-        val handCards = getCardData(handData)
-        val pileCards = getCardData(pileData)
+      val name = playerData(0)
+      val difficulty = playerData(1)
+      val points = playerData(2).toInt
+      val sweeps = playerData(3).toInt
 
-        val player = difficulty match
-          case human if human == "None" =>
-            Player(game, name)
-          case bot if Array("1", "2", "3").contains(bot) =>
-            AIPlayer(game, name, difficulty.toInt)
-          case _ => throw CorruptedSaveException()
+      val handCards = getCardData(handData)
+      val pileCards = getCardData(pileData)
 
-        player.points = points
-        player.sweeps = sweeps
-        player.hand ++= handCards
-        player.pile ++= pileCards
+      val player = difficulty match
+        case human if human == "None" =>
+          Player(game, name)
+        case bot if Array("1", "2", "3").contains(bot) =>
+          AIPlayer(game, name, difficulty.toInt)
+        case _ => throw CorruptedSaveException("Difficulty of bot is wrong.")
 
-        players += player
-      source.close()
+      player.points = points
+      player.sweeps = sweeps
+      player.hand ++= handCards
+      player.pile ++= pileCards
+
+      players += player
+    source.close()
 
 
-      game.addPlayers(players.toSeq)
-      game.table ++= tableCards
-      game.deck.setCards(deckCards)
-      game.turnCount = turnCount
-      game.dealerCount = dealerCount
-      game.roundNumber = roundNumber
-      game.currentPlayer.setAllPossibleMoves()
-      game.lastCardTaker = lastCardTaker match
-        case nobody if nobody == "None" =>
-          None
-        case player =>
-          players.find( x => x.name == player )
-
-      Some(game)
-
-    catch
-      case e: CorruptedSaveException =>
-        println("Save corrupted or something lol")
+    game.addPlayers(players.toSeq)
+    game.table ++= tableCards
+    game.deck.setCards(deckCards)
+    game.turnCount = turnCount
+    game.dealerCount = dealerCount
+    game.roundNumber = roundNumber
+    game.currentPlayer.setAllPossibleMoves()
+    game.lastCardTaker = lastCardTaker match
+      case nobody if nobody == "None" =>
         None
+      case player =>
+        val foundPlayer = players.find( x => x.name == player )
+        foundPlayer match
+          case Some(guy) => Some(guy)
+          case None => throw CorruptedSaveException("Player marked as last card taker not found in game.")
+
+    Some(game)
